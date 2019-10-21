d44a1577610cc2f3020624a8ee80f1c9b3b61795
reepz <a.wojtowicz@outlook.com>
2019-10-21 17:42:25 +0200

    finished layout, uploading, enabled registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   resources :projects
   devise_for :users
-  root "static_pages#about"
+  root "projects#index"
 
   get '/kontakt/', to: "static_pages#contact"
-  get '/o_nas/', to: "static_pages#about"
+  get '/o_nas/', to: "projects#index"
   get '/nasi_klienci/', to: "static_pages#our_clients"
   get '/oferta/', to: "static_pages#offer"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
